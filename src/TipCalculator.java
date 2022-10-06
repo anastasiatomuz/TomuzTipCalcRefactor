f710b5ba4a4641048235618b70b835d189dc255c
student <sample@bths.edu>
2022-10-06 10:52:37 -0400

    first work part commit

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,7 +4,10 @@ import java.text.DecimalFormat;
 public class TipCalculator {
    /*
 
-    Your TipCalculator class should have the following methods that calculate and return the values described below; you should RETURN all values -- do not add print statements in your TipCalculator class, all printing should be handled by the code in your main method (as we will learn later, it's best practice to have your program's "logic" in one class, and input/output in another):
+    Your TipCalculator class should have the following methods that calculate and return the values described below; you should RETURN all values
+    -- do not add print statements in your TipCalculator class, all printing should be handled by the code in your main method (as we will learn later,
+    it's best practice to have your program's "logic" in one class, and input/output in another):
+
 getTotalBillBeforeTip():  getter method that returns the totalBillBeforeTip instance variable
 getTipPercentage():  getter method that returns the tipPercentage instance variable
 addMeal(double cost): method that increments totalBillBeforeTip by cost and returns no value
@@ -17,12 +20,18 @@ perPersonTotalCost(): method that calculates and returns a double representing t
     */
     private int numPeople;
     private int tipPercentage;
+    //new comment
     private double totalBillBeforeTip;
 
     public TipCalculator(int numPeople, int tipPercentage){
         this.numPeople = numPeople;
         this.tipPercentage = tipPercentage;
         totalBillBeforeTip = 0;
+    }
+
+    //getter method for totalBillBeforeTip
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
     }
 
 }
