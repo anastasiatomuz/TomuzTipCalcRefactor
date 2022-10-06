6bc6c3eccc8f70d7ab7e2bb5666804f7a8dc9535
student <sample@bths.edu>
2022-10-06 11:02:52 -0400

    commit 2

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -20,7 +20,6 @@ perPersonTotalCost(): method that calculates and returns a double representing t
     */
     private int numPeople;
     private int tipPercentage;
-    //new comment
     private double totalBillBeforeTip;
 
     public TipCalculator(int numPeople, int tipPercentage){
@@ -29,9 +28,38 @@ perPersonTotalCost(): method that calculates and returns a double representing t
         totalBillBeforeTip = 0;
     }
 
-    //getter method for totalBillBeforeTip
+    //getter method that returns the totalBillBeforeTip instance variable
     public double getTotalBillBeforeTip(){
         return totalBillBeforeTip;
     }
 
+    //getter method that returns the tipPercentage instance variable
+    public int getTipPercentage(){
+        return tipPercentage;
+    }
+
+    //method that increments totalBillBeforeTip by cost and returns no value
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    }
+
+    //method that calculates and returns a double representing the total tip amount added to the bill
+    public double tipAmount(){
+        return (tipPercentage / 100) * totalBillBeforeTip;
+    }
+
+    //method that calculates and returns a double representing the per person cost before tip
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip / numPeople;
+    }
+
+    //method that calculates and returns a double representing the per person tip amount
+    public double perPersonTipAmount(){
+        return 5.5;
+    }
+
+    //method that calculates and returns a double representing the total cost per person
+    public double perPersonTotalCost(){
+        return 5.5;
+    }
 }
